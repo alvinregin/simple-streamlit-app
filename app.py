221af8d2ce18ff9e47aa5457a34d70d0fa7bebc6
Alvin Regin <alvinregin2017@gmail.com>
2025-07-04 02:46:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.title("Simple Streamlit Cloud App")
+st.title("Simple Streamlit App")
 
 name = st.text_input("Enter your name:")
 
 if name:
-    st.write(f"Hello, {name}! Welcome to Streamlit Cloud.")
+    st.write(f"Hello, {name}! Welcome to Streamlit.")
 else:
     st.write("Please enter your name above.")
